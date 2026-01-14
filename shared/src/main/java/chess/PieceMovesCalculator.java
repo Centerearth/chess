992edc6c65e07d8fe2a12edb7c74763534ccd17e
Centerearth <andrewtheamazingbrother@gmail.com>
2026-01-13 21:13:41 -0700

    Added a PieceMovesCalculator interface

---
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -0,0 +1,12 @@
+package chess;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Calculates the possible moves for a given piece
+ */
+
+public interface PieceMovesCalculator {
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition);
+}

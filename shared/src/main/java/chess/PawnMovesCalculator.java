b416a797980b017b557b4455cc92eeec6c408981
Centerearth <andrewtheamazingbrother@gmail.com>
2026-01-16 12:26:38 -0700

    Refactored again in PawnMovesCalculator

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -10,85 +10,55 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
 
         ChessGame.TeamColor currentColor = board.getPiece(myPosition).getTeamColor();
         ArrayList<ChessMove> validMoves = new ArrayList<>();
-        int row = myPosition.getRow();
-        int col = myPosition.getColumn();
 
         if (currentColor == ChessGame.TeamColor.WHITE) {
-            ChessPosition nextSquare = new ChessPosition(row+1, col);
-            if (board.getPiece(nextSquare) == null) {
-                if (row == 7) {
-                    this.promotion(validMoves, myPosition, nextSquare);
-                } else {
-                    validMoves.add(new ChessMove(myPosition, nextSquare, null));
-                    if (row == 2) {
-                        ChessPosition twoSquare = new ChessPosition(row + 2, col);
-                        if (board.getPiece(twoSquare) == null) {
-                            validMoves.add(new ChessMove(myPosition, twoSquare, null));
-                        }
-                    }
-                }
-            }
-            if (col != 1) {
-                ChessPosition firstDiagonal = new ChessPosition(row + 1, col - 1);
-                ChessPiece diagonalPiece = board.getPiece(firstDiagonal);
-                if (diagonalPiece != null && diagonalPiece.getTeamColor() != currentColor) {
-                    if (row == 7) {
-                        this.promotion(validMoves, myPosition, firstDiagonal);
-                    } else {
-                        validMoves.add(new ChessMove(myPosition, firstDiagonal, null));
-                    }
-                }
-            }
-            if (col != 8) {
-                ChessPosition secondDiagonal = new ChessPosition(row + 1, col + 1);
-                ChessPiece diagonalPiece = board.getPiece(secondDiagonal);
-                if (diagonalPiece != null && diagonalPiece.getTeamColor() != currentColor) {
-                    if (row == 7) {
-                        this.promotion(validMoves, myPosition, secondDiagonal);
-                    } else {
-                        validMoves.add(new ChessMove(myPosition, secondDiagonal, null));
-                    }
-                }
-            }
+            pawnMoves(currentColor, validMoves, board, myPosition, 2, 7, 1);
         } else {
-            ChessPosition nextSquare = new ChessPosition(row-1, col);
-            if (board.getPiece(nextSquare) == null) {
-                if (row == 2) {
-                    this.promotion(validMoves, myPosition, nextSquare);
-                } else {
-                    validMoves.add(new ChessMove(myPosition, nextSquare, null));
-                    if (row == 7) {
-                        ChessPosition twoSquare = new ChessPosition(row - 2, col);
-                        if (board.getPiece(twoSquare) == null) {
-                            validMoves.add(new ChessMove(myPosition, twoSquare, null));
-                        }
-                    }
-                }
-            }
-            if (col != 1) {
-                ChessPosition firstDiagonal = new ChessPosition(row - 1, col - 1);
-                ChessPiece diagonalPiece = board.getPiece(firstDiagonal);
-                if (diagonalPiece != null && diagonalPiece.getTeamColor() != currentColor) {
-                    if (row == 2) {
-                        this.promotion(validMoves, myPosition, firstDiagonal);
-                    } else {
-                        validMoves.add(new ChessMove(myPosition, firstDiagonal, null));
-                    }
-                }
-            }
-            if (col != 8) {
-                ChessPosition secondDiagonal = new ChessPosition(row - 1, col + 1);
-                ChessPiece diagonalPiece = board.getPiece(secondDiagonal);
-                if (diagonalPiece != null && diagonalPiece.getTeamColor() != currentColor) {
-                    if (row == 2) {
-                        this.promotion(validMoves, myPosition, secondDiagonal);
-                    } else {
-                        validMoves.add(new ChessMove(myPosition, secondDiagonal, null));
+            pawnMoves(currentColor, validMoves, board, myPosition, 7, 2, -1);
+        }
+        return validMoves;
+    }
+
+    public void pawnMoves(ChessGame.TeamColor currentColor, ArrayList<ChessMove> validMoves, ChessBoard board, ChessPosition myPosition, int secondRow, int secondToLastRow, int factor) {
+
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+        ChessPosition nextSquare = new ChessPosition(row+factor, col);
+        if (board.getPiece(nextSquare) == null) {
+            if (row == secondToLastRow) {
+                this.promotion(validMoves, myPosition, nextSquare);
+            } else {
+                validMoves.add(new ChessMove(myPosition, nextSquare, null));
+                if (row == secondRow) {
+                    ChessPosition twoSquare = new ChessPosition(row + factor * 2, col);
+                    if (board.getPiece(twoSquare) == null) {
+                        validMoves.add(new ChessMove(myPosition, twoSquare, null));
                     }
                 }
             }
         }
-        return validMoves;
+        if (col != 1) {
+            ChessPosition firstDiagonal = new ChessPosition(row + factor, col - 1);
+            ChessPiece diagonalPiece = board.getPiece(firstDiagonal);
+            if (diagonalPiece != null && diagonalPiece.getTeamColor() != currentColor) {
+                if (row == secondToLastRow) {
+                    this.promotion(validMoves, myPosition, firstDiagonal);
+                } else {
+                    validMoves.add(new ChessMove(myPosition, firstDiagonal, null));
+                }
+            }
+        }
+        if (col != 8) {
+            ChessPosition secondDiagonal = new ChessPosition(row + factor, col + 1);
+            ChessPiece diagonalPiece = board.getPiece(secondDiagonal);
+            if (diagonalPiece != null && diagonalPiece.getTeamColor() != currentColor) {
+                if (row == secondToLastRow) {
+                    this.promotion(validMoves, myPosition, secondDiagonal);
+                } else {
+                    validMoves.add(new ChessMove(myPosition, secondDiagonal, null));
+                }
+            }
+        }
     }
 
     public void promotion(ArrayList<ChessMove> validMoves, ChessPosition myPosition, ChessPosition nextSquare) {

50dcbb3f894ceeabbc07efb5ccc6af72347c2c10
Centerearth <andrewtheamazingbrother@gmail.com>
2026-01-22 19:25:17 -0700

    Wrote a helper function for isInCheck

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -153,15 +153,22 @@ public class ChessGame implements Cloneable {
         ChessPosition kingPosition = findKingPosition(teamColor);
         for (int i = 1; i <= 8; i++ ) {
             for (int j = 1; j <= 8; j++) {
-                ChessPosition position = new ChessPosition(i, j);
-                ChessPiece piece = board.getPiece(position);
-                if (piece != null && piece.getTeamColor() != teamColor) {
-                    Collection<ChessMove> validMoves = piece.pieceMoves(board, position);
-                    for (ChessMove move: validMoves) {
-                        if (move.getEndPosition().equals(kingPosition)) {
-                            return true;
-                        }
-                    }
+                if (checkHelper(teamColor, kingPosition, i, j)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkHelper(TeamColor teamColor, ChessPosition kingPosition, int i, int j) {
+        ChessPosition position = new ChessPosition(i, j);
+        ChessPiece piece = board.getPiece(position);
+        if (piece != null && piece.getTeamColor() != teamColor) {
+            Collection<ChessMove> validMoves = piece.pieceMoves(board, position);
+            for (ChessMove move: validMoves) {
+                if (move.getEndPosition().equals(kingPosition)) {
+                    return true;
                 }
             }
         }

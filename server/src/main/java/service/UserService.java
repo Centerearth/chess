2b8ff00f135c46d2e60dbe6c9f6f6136016bd157
Centerearth <andrewtheamazingbrother@gmail.com>
2026-02-05 09:41:02 -0700

    Added logic for register

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,16 +1,41 @@
 package service;
 
+import dataaccess.*
+import dataaccess.UserDataAccess;
 import model.AuthData;
+import model.UserData;
 import recordandrequest.*;
 
+import java.util.UUID;
+
 public class UserService {
+    private MemoryUserDataAccess userDataAccess = new MemoryUserDataAccess();
+    private MemoryAuthDataAccess authDataAccess = new MemoryAuthDataAccess();
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
+    }
+
     public RegisterResult register(RegisterRequest registerRequest) {
         if (registerRequest.username().isBlank() || registerRequest.password().isBlank()
         || registerRequest.email().isBlank()) {
             throw new BadRequestException("The fields cannot be left blank");
         }
-        return new RegisterResult("placeholder", new AuthData("placeholder", "placeholder"));
+
+        if (userDataAccess.getUser(registerRequest.username()) != null) {
+            throw new AlreadyTakenException("This username is already taken.");
+        } else {
+            UserData newUserData = new UserData(registerRequest.username(),
+                    registerRequest.password(), registerRequest.email());
+            AuthData newAuthData = new AuthData(generateToken(), registerRequest.username());
+
+            userDataAccess.addUserData(newUserData);
+            authDataAccess.addAuthData(newAuthData);
+            return new RegisterResult(registerRequest.username(), newAuthData);
+        }
+
     }
+
     //public LoginResult login(LoginRequest loginRequest) {}
     //public void logout(LogoutRequest logoutRequest) {}
 }

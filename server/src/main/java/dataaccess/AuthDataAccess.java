4429cfdfa9238996560cd72e17c9eb4361a65b3b
Centerearth <andrewtheamazingbrother@gmail.com>
2026-02-05 09:50:31 -0700

    Added implementation for AuthData

--- server/src/main/java/dataaccess/AuthDataAccess.java
+++ server/src/main/java/dataaccess/AuthDataAccess.java
@@ -1,4 +1,10 @@
 package dataaccess;
 
+import model.AuthData;
+
 public interface AuthDataAccess {
+    void addAuthData(AuthData newAuth);
+    AuthData getAuth(String authToken);
+    void deleteAuth(String authToken);
+    void removeAllAuthData();
 }
--- server/src/main/java/dataaccess/MemoryAuthDataAccess.java
+++ server/src/main/java/dataaccess/MemoryAuthDataAccess.java
@@ -1,4 +1,24 @@
 package dataaccess;
 
+import model.AuthData;
+import model.UserData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MemoryAuthDataAccess implements AuthDataAccess {
+    private HashMap<String, AuthData> allAuthData;
+
+    public void addAuthData(AuthData newAuth) {
+        allAuthData.put(newAuth.authToken(), newAuth);
+    }
+    public AuthData getAuth(String authToken) {
+        return allAuthData.getOrDefault(authToken, null);
+    }
+    public void deleteAuth(String authToken) {
+        allAuthData.remove(authToken);
+    }
+    public void removeAllAuthData() {
+        allAuthData.clear();
+    }
 }

2298488540c77cbe54f462e30373789471e5f275
Centerearth <andrewtheamazingbrother@gmail.com>
2026-02-05 09:23:07 -0700

    Passed bad request test for register

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,10 +1,15 @@
 package service;
 
+import model.AuthData;
 import recordandrequest.*;
 
 public class UserService {
     public RegisterResult register(RegisterRequest registerRequest) {
-
+        if (registerRequest.username().isBlank() || registerRequest.password().isBlank()
+        || registerRequest.email().isBlank()) {
+            throw new BadRequestException("The fields cannot be left blank");
+        }
+        return new RegisterResult("placeholder", new AuthData("placeholder", "placeholder"));
     }
     //public LoginResult login(LoginRequest loginRequest) {}
     //public void logout(LogoutRequest logoutRequest) {}

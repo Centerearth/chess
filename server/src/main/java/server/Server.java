32a4fb0cc0680b91e76b0876c47b9c0307604015
Centerearth <andrewtheamazingbrother@gmail.com>
2026-02-09 18:44:46 -0700

    Added the rest of the methods in

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,23 +1,31 @@
 package server;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
 import io.javalin.*;
 import io.javalin.http.Context;
 import recordandrequest.*;
+import service.GameService;
 import service.UserService;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Server {
 
     private final Javalin javalin;
     private final UserService userService = new UserService();
+    private final GameService gameService = new GameService();
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"))
                 .delete("/db", this::clearApplication)
                 .delete("/session", this::logoutUser)
+                .post("/session", this::loginUser)
                 .post("/user", this::createNewUser)
+                .post("/game", this::createNewGame)
+                .put("/game", this::joinGame)
+                .get("/game", this::listGames)
                 .start();
 
         // Register your endpoints and exception handlers here.
@@ -62,6 +70,56 @@ public class Server {
             userService.logout(new LogoutRequest(authToken));
         } catch (Exception e) { // change all this
             System.out.println("Hi");
+        }
+    }
+
+    private void createNewGame(Context context) {
+        try {
+            HashMap<String, String> bodyObject = getBodyObject(context, HashMap.class);
+            String gameName = bodyObject.get("gameName");
+            String authToken = getAuthToken(context);
+            CreateGameResult createGameResult = gameService.createGame(new CreateGameRequest(authToken,
+                    gameName));
+
+            String json = new Gson().toJson(createGameResult);
+            context.json(json);
+        } catch (Exception e) {
+            System.out.println("hi3");
+        }
+
+    }
+
+    private void joinGame(Context context) {
+        try {
+            HashMap<String, Object> bodyObject = getBodyObject(context, HashMap.class);
+            String teamColorString = bodyObject.get("playerColor").toString();
+            ChessGame.TeamColor teamColor = null;
+            if (teamColorString.equals("WHITE")) {
+                teamColor = ChessGame.TeamColor.WHITE;
+            } else if (teamColorString.equals("BLACK")) {
+                teamColor = ChessGame.TeamColor.BLACK;
+            }
+
+            String authToken = getAuthToken(context);
+            int gameID = (int) (double) bodyObject.get("gameID"); //why does it convert to double?
+            System.out.println(gameID);
+            gameService.joinGame(new JoinGameRequest(authToken,
+                    teamColor, gameID));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    private void listGames(Context context) {
+        try {
+            String authToken = getAuthToken(context);
+            ListGameResult listGameResult = gameService.getAllGameData(new ListGameRequest(authToken));
+
+            String json = new Gson().toJson(listGameResult);
+            context.json(json);
+        } catch (Exception e) { // change all this
+            System.out.println(e);
         }
     }
 

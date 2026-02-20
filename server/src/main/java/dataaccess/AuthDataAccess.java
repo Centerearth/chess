ab9bd8a138808aa54b4b0c136f056b8397b52c55
Centerearth <andrewtheamazingbrother@gmail.com>
2026-02-19 20:29:05 -0700

    Implemented AuthDataAccess

--- server/src/main/java/dataaccess/AuthDataAccess.java
+++ server/src/main/java/dataaccess/AuthDataAccess.java
@@ -3,8 +3,8 @@ package dataaccess;
 import model.AuthData;
 
 public interface AuthDataAccess {
-    void addAuthData(AuthData newAuth);
-    AuthData getAuth(String authToken);
-    void deleteAuth(String authToken);
-    void removeAllAuthData();
+    void addAuthData(AuthData newAuth) throws DataAccessException;
+    AuthData getAuth(String authToken)  throws DataAccessException;
+    void deleteAuth(String authToken)  throws DataAccessException;
+    void removeAllAuthData() throws DataAccessException;
 }
---
+++ server/src/main/java/dataaccess/SQLAuthDataAccess.java
@@ -0,0 +1,80 @@
+package dataaccess;
+
+import chess.ChessGame;
+import com.google.gson.Gson;
+import model.AuthData;
+import model.GameData;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static dataaccess.DatabaseManager.*;
+
+public class SQLAuthDataAccess implements AuthDataAccess {
+    public SQLAuthDataAccess() {
+        createDatabase();
+    }
+
+    public void addAuthData (AuthData newAuth) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+
+            var serializer = new Gson();
+            var authJSON = serializer.toJson(newAuth);
+
+            try (var preparedStatement = conn.prepareStatement("INSERT INTO auth (authToken, authData) VALUES(?, ?)")) {
+                conn.setCatalog("chess");
+                preparedStatement.setString(1, newAuth.authToken());
+                preparedStatement.setString(2, authJSON);
+                preparedStatement.executeUpdate();
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Error: the auth failed to add", e);
+        }
+    }
+
+
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("SELECT authData FROM auth WHERE authToken=?")) {
+                preparedStatement.setString(1, authToken);
+                conn.setCatalog("chess");
+
+                try (var rs = preparedStatement.executeQuery()) {
+                    rs.next();
+                    var authDataString = rs.getString("authData");
+                    return new Gson().fromJson(authDataString, AuthData.class);
+                }
+            }
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+
+
+    public void deleteAuth(String authToken) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            conn.setCatalog("chess");
+            try (var preparedStatement = conn.prepareStatement("DELETE FROM auth WHERE authToken=?")) {
+                preparedStatement.setString(1, authToken);
+                preparedStatement.executeUpdate();
+            }
+
+        } catch (Exception e) {
+            throw new DataAccessException("Error: failed to remove the auth data", e);
+        }
+    }
+
+    public void removeAllAuthData() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+
+            conn.setCatalog("chess");
+            var statement = "TRUNCATE TABLE auth";
+            var preparedStatement = conn.prepareStatement(statement);
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            throw new DataAccessException("Error: the auth data was not deleted", e);
+        }
+    }
+
+}

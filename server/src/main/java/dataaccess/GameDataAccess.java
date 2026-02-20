7aaaa4433e3c738a23f65e0e25adfc61889c5949
Centerearth <andrewtheamazingbrother@gmail.com>
2026-02-19 17:03:08 -0700

    Updated the GameDataAccess interface to properly reflect how it is being using from both implementations. Still need to fix how SQLGameAccess handles errors

--- server/src/main/java/dataaccess/GameDataAccess.java
+++ server/src/main/java/dataaccess/GameDataAccess.java
@@ -1,4 +1,15 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.GameData;
+import java.util.ArrayList;
+
+
 public interface GameDataAccess {
+    public void addGameData (GameData newGame);
+    public GameData getGame(int gameID);
+    public void removeGameData(int gameID);
+    public void removeAllGameData();
+    public ArrayList<GameData> getAllGameData();
+    public void updateGame(ChessGame.TeamColor teamColor, int gameID, String username);
 }

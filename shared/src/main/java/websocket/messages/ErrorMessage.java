731a622e66644caa76d9fb5fdf90be2d934119f5
Centerearth <andrewtheamazingbrother@gmail.com>
2026-03-28 20:32:16 -0600

    ErrorMessage now running

---
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -0,0 +1,33 @@
+package websocket.messages;
+
+import java.util.Objects;
+
+public class ErrorMessage extends ServerMessage {
+    private final String message;
+    public ErrorMessage(ServerMessageType type, String message) {
+        super(type);
+        this.message = message;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        ErrorMessage that = (ErrorMessage) o;
+        return Objects.equals(getMessage(), that.getMessage());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getMessage());
+    }
+}

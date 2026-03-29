dae6e52ba6e803cd9931d420de484076de6cc8c4
Centerearth <andrewtheamazingbrother@gmail.com>
2026-03-28 20:57:30 -0600

    Connection manager takes care of the active sessions, keyed to their game IDs. need to return to properly convert to JSON

---
+++ server/src/main/java/server/ConnectionManager.java
@@ -0,0 +1,41 @@
+package server;
+
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    public final ConcurrentHashMap<Integer, HashSet<Session>> allConnections = new ConcurrentHashMap<>();
+
+    public void add(int gameID, Session session) {
+        if (allConnections.get(gameID) == null) {
+            addNewGame(gameID);
+        }
+        allConnections.get(gameID).add(session);
+    }
+
+    private void addNewGame(int gameID) {
+        allConnections.put(gameID, new HashSet<Session>());
+    }
+
+    public void remove(int gameID) {
+        allConnections.remove(gameID);
+    }
+
+    public void broadcast(Session excludeSession, ServerMessage serverMessage, int gameID) throws IOException {
+        //String msg = notification.toString();
+        //this needs to be a JSON
+        String msg = "";
+        HashSet<Session> connections = allConnections.get(gameID);
+        for (Session s : connections) {
+            if (s.isOpen()) {
+                if (!s.equals(excludeSession)) {
+                    s.getRemote().sendString(msg);
+                }
+            }
+        }
+    }
+}
